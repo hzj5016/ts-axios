e088d55dca1d52a670e039571bcf1b009e6b2ebe
hzj5016 <hzj5016@gmail.com>
2019-09-01 22:50:33 +0800

    feat(src/index.ts): processUrl
    在发送请求之前处理传入的params选项, 拼接到url上

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { AxiosRequestConfig, Method } from './types/index'
 import Xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   Xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = processUrl(config)
+}
+
+function processUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
